ce00f0923383c35f8f3b73358ba2806081a8d5c0
ke1echi <mrkelechichinaka@gmail.com>
2020-07-26 06:58:39 +0100

    Made some changes to file

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var http = require('http');
 var url = require('url');
 
-// Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   var queryData = url.parse(request.url, true).query;
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	
+
   if (queryData.name) {
     // user told us their name in the GET request, ex: http://host:3000/?name=Tom
 		response.end(`Hello ${queryData.name}, Welcome to WeJapa Internships\n`);
